ed23ae2a783d66e4bed2ecb9560515cc894c7fe0
savigurjar <savigurjar1201@gmail.com>
2025-11-02 10:14:51 +0530

    clerk auth

--- 1.intro to nextjs/my-app/app/page.tsx
+++ 1.intro to nextjs/my-app/app/page.tsx
@@ -1,10 +1,11 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import "dotenv/config"
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen ">
-      One Piece
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-2xl font-bold mb-4">One Piece</h1>
       <Button>Hello World!</Button>
     </div>
   );
