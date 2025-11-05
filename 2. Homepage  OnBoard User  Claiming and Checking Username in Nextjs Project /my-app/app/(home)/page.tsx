fed629b777abc43c99c9eefcba807b94f03d9756
savigurjar <savigurjar1201@gmail.com>
2025-11-05 14:49:06 +0530

    front

--- 2. Homepage  OnBoard User  Claiming and Checking Username in Nextjs Project /my-app/app/(home)/page.tsx
+++ 2. Homepage  OnBoard User  Claiming and Checking Username in Nextjs Project /my-app/app/(home)/page.tsx
@@ -2,12 +2,14 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import "dotenv/config"
 import { onboardUser } from "@/modules/auth/actions";
+import "../globals.css";
+
 
 const HomePage = async()=> {
   await onboardUser()
   return (
     <div >
-      Homepage
+      {/* Homepage */}
     </div>
   );
 }
--- 2. Homepage  OnBoard User  Claiming and Checking Username in Nextjs Project /my-app/modules/auth/actions/index.ts
+++ 2. Homepage  OnBoard User  Claiming and Checking Username in Nextjs Project /my-app/modules/auth/actions/index.ts
@@ -35,6 +35,7 @@ export const onboardUser = async () => {
       },
     });
 
+
     return {
       success: true,
       user: newUser,
